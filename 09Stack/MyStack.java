e772741c7f116d7aa8bd39aeda23e9a1efbb4899
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2015-03-20 18:29:16 +0000

    MyStack generics and push method

--- 09Stack/MyStack.java
+++ 09Stack/MyStack.java
@@ -1,9 +1,18 @@
-public class MyStack {
+public class MyStack<T> {
 
-    LNode head;
+    LNode<T> head;
     
     public boolean empty() {
         return head == null;
+    }
+
+    public T push(T item) {
+        LNode<T> ln = new LNode<T>(item);
+        if (head != null)
+            ln.setNext(head);
+
+        head = ln;
+        return item;
     }
 
 }
