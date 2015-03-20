868fcbbbea4c5c23b21792d8425418330ec392cd
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2015-03-20 18:19:19 +0000

    copies LNode from LinkedList

---
+++ 09Stack/LNode.java
@@ -0,0 +1,28 @@
+public class LNode<T> {
+
+    private T data;
+    private LNode<T> next;
+
+    public LNode() { }
+
+    public LNode(T t) {
+        setData(t);
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public LNode<T> getNext() {
+        return next;
+    }
+
+    public void setData(T t) {
+        data = t;
+    }
+
+    public void setNext(LNode<T> ln) {
+        next = ln;
+    }
+
+}
