09b00d424f4a0d802d913c9bc75c0f7bca31f205
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2015-03-23 18:32:21 +0000

    MyQueue adds constructor and tests

--- 09Stack/MyQueue.java
+++ 09Stack/MyQueue.java
@@ -2,6 +2,10 @@ public class MyQueue<T> {
 
     MyLinkedList<T> mll;
 
+    public MyQueue() {
+        mll = new MyLinkedList<T>();
+    }
+
     public boolean enqueue(T e) {
         mll.add(e);
         return true;
@@ -9,6 +13,16 @@ public class MyQueue<T> {
 
     public T dequeue() {
         return mll.remove();
+    }
+
+    public static void main(String[] args) {
+        MyQueue<Integer> q = new MyQueue<Integer>();
+        System.out.println( q.enqueue(1) );
+        System.out.println( q.enqueue(2) );
+        System.out.println( q.dequeue() );
+        System.out.println( q.enqueue(3) );
+        System.out.println( q.dequeue() );
+        System.out.println( q.dequeue() );
     }
 
 }
