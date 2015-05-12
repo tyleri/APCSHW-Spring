c465bc4e4a2d45dd77f7ed90c44f1cdf27c14539
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2015-05-12 12:10:09 -0400

    Heap: creates method headers in MyHeap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,3 +1,23 @@
 public class MyHeap {
 
+    int[] heap;
+
+    public MyHeap() {}
+
+    public MyHeap(boolean isMax) {}
+
+    public String toString() {
+        return "";
+    }
+
+    public int remove() {
+        return 0;
+    }
+
+    public void add(int n) {
+    }
+
+    public int peek() {
+        return heap[1];
+    }
 }
