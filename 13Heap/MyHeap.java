193d7d9d700abfa9ba69620ce27803f510c41a76
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2015-05-12 12:33:21 -0400

    Heap: completes add method

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MyHeap {
 
     private int[] heap;
@@ -16,7 +18,7 @@ public class MyHeap {
     }
 
     public String toString() {
-        return "";
+        return Arrays.toString(heap);
     }
 
     public int remove() {
@@ -24,9 +26,41 @@ public class MyHeap {
     }
 
     public void add(int n) {
+        if (!hasRoot) {
+            heap[1] = n;
+            hasRoot = true;
+        } else if (isMaxHeap) {
+            int index = heap[0] + 1; // next index in heap to fill in
+            while ( index != 1 && n > heap[index/2]) {
+                heap[index] = heap[index/2];
+                index /= 2;
+            }
+            heap[index] = n;
+        } else {
+            int index = heap[0] + 1; // next index in heap to fill in
+            while (index != 1 && n < heap[index/2]) {
+                heap[index] = heap[index/2];
+                index /= 2;
+            }
+            heap[index] = n;
+        }
+
+        heap[0]++;
     }
 
     public int peek() {
         return heap[1];
+    }
+
+    public static void main(String[] args) {
+        MyHeap mh = new MyHeap();
+        mh.add(5);
+        System.out.println(mh);
+        mh.add(19);
+        System.out.println(mh);
+        mh.add(23);
+        System.out.println(mh);
+        mh.add(1);
+        System.out.println(mh);
     }
 }
