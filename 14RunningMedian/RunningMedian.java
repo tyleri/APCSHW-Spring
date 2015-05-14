5e080b07e9a5cc07c82c09128ca7f61447f91212
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2015-05-14 12:29:47 -0400

    RunningMedian: completes add method

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -8,7 +8,22 @@ public class RunningMedian {
     }
 
     public void add(int value) {
-
+        if (left.size() + right.size() == 0) {
+            left.add(value);
+        } else if (left.size() + right.size() == 1) {
+            if (value < left.peek()) {
+                left.add(value);
+                value = left.remove();
+            }
+            right.add(value);
+        } else {
+            int median = getMedian();
+            if (value > median) {
+                right.add(value);
+            } else {
+                left.add(value);
+            }
+        }
     }
 
     public double getMedian() {
@@ -25,4 +40,20 @@ public class RunningMedian {
         }
     }
 
+    public static void main(String args) {
+        RunningMedian rm = new RunningMedian();
+        if (args.length == 0) {
+            rm.add(3);
+            System.out.println(rm.getMedian());
+            rm.add(5);
+            System.out.println(rm.getMedian());
+            rm.add(9);
+            System.out.println(rm.getMedian());
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                rm.add(args[i]);
+                System.out.println("Median: " + rm.getMedian());
+            }
+        }
+    }
 }
