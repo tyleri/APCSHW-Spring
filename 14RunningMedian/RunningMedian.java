3c1b41a91b185b703f9d8ba6786e698e506d13a7
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2015-05-14 12:03:19 -0400

    RunningMedian: creates constructor

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -2,6 +2,11 @@ public class RunningMedian {
 
     private MyHeap left, right;
 
+    public RunningMedian() {
+        left = new MyHeap();
+        right = new MyHeap(false);
+    }
+
     public void add(int value) {
 
     }
