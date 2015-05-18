0063817108053ff073a43ffecebb2a68b322c652
tyleri <tyler.ishikawa@outlook.com>
2015-05-18 18:13:29 -0400

    HeapSort: separates pushDown into separate method

--- 15HeapSort/HeapSort.java
+++ 15HeapSort/HeapSort.java
@@ -8,30 +8,33 @@ public class HeapSort {
 
     public void sort() {
         heapify();
+
+        int end = arr.length-1;
     }
 
     private void heapify() {
         int index = arr.length / 2 - 1; // first element that needs to be pushed down
-
-        int biggestChild;
         for (int i = index; i >= 0; i--) { // push down each element
+            pushDown(i);
+        }
+    }
 
-            for (int j = i; j <= index; j++) { // keep pushing it down to the right place
-                
-                // if the element only has one child or left child > right child
-                if (j*2+2 == arr.length || arr[j*2+1] > arr[j*2+2]) {
-                    biggestChild = j*2+1;
-                } else { // if right child > left child
-                    biggestChild = j*2+2;
-                }
+    private void pushDown(int index) {
+        int biggestChild;
+        for (int j = index; j <= arr.length/2-1; j++) { // keep pushing element down to the right place
 
-                // if the biggest of the children is bigger than the element, swap
-                if (arr[j] < arr[biggestChild]) {
-                    int temp = arr[j];
-                    arr[j] = arr[biggestChild];
-                    arr[biggestChild] = temp;
-                }
+            // if the element only has one child or left child > right child
+            if (j*2+2 == arr.length || arr[j*2+1] > arr[j*2+2]) {
+                biggestChild = j*2+1;
+            } else { // if right child > left child
+                biggestChild = j*2+2;
+            }
 
+            // if the biggest of the children is bigger than the element, swap
+            if (arr[j] < arr[biggestChild]) {
+                int temp = arr[j];
+                arr[j] = arr[biggestChild];
+                arr[biggestChild] = temp;
             }
         }
     }
